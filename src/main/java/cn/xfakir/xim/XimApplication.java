0fc25487dca6faebfcdb68321efad0ec7c83679b
xfakir <490679728@qq.com>
2020-09-15 19:40:43 +0800

    Initial commit

---
+++ src/main/java/cn/xfakir/xim/XimApplication.java
@@ -0,0 +1,13 @@
+package cn.xfakir.xim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xfakir/xim/XimApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xfakir.xim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
